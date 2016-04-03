f4bc671f4dd78295313b20829d6e2a14e88b896c
AbbinVarghese <paadam2@gmail.com>
2016-04-03 13:22:50 +0530

    Initial Commit

---
+++ AsstetsSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AsstetsSample
+//
+//  Created by Abbin Varghese on 03/04/16.
+//  Copyright © 2016 Abbin Varghese. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AsstetsSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AsstetsSample
+//
+//  Created by Abbin Varghese on 03/04/16.
+//  Copyright © 2016 Abbin Varghese. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
